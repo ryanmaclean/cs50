710a2f15e6b6dd562a5db21777dfc058dbd9a3bd
Ryan MacLean <ryan@ryanmaclean.com>
2015-08-09 17:01:35 -0700

    Added condition 0 source code

---
+++ week1/condition0.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+int main(void){
+	printf("I'd like an integer please: ");
+	int n = GetInt();
+
+	if (n > 0){
+		printf("You picked a positive number!\n");
+	}
+	else if (n < 0){
+		printf("You picked a negative number!\n");
+	}
+	else {
+		printf("You picked zero!\n");
+	}
+}
