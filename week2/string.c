44563e701becc2cc3f89dfe78add429fa217bb40
Ryan MacLean <ryan@ryanmaclean.com>
2015-08-09 18:35:19 -0700

    unoptimized string algo

--- week2/string.c
+++ week2/string.c
@@ -1,9 +1,15 @@
 #import <stdio.h>
 #import <cs50.h>
+#import <string.h>
 
 int main(void){
 	string s = GetString();
-	for (int i = 0; i < 6; i++){
-		printf("%c\n", s[i]);
+	if (s != NULL){
+
+		for (int i = 0; i < strlen(s); i++){
+			printf("%c\n", s[i]);
+		}
 	}
+	else
+		printf("Please type a name :)\n");
 }
