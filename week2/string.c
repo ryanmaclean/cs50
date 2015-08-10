6989eea741b77aa1992aaabb3da93a097e8ee32b
Ryan MacLean <ryan@ryanmaclean.com>
2015-08-09 18:46:50 -0700

    Fixed logical error

--- week2/string.c
+++ week2/string.c
@@ -3,9 +3,9 @@
 #import <string.h>
 
 int main(void){
+	printf("Type a name, and we'll print it one character at a time!\n");
 	string s = GetString();
 	if (s != NULL){
-
 		for (int i = 0, n= strlen(s); i < n; i++){
 			printf("%c\n", s[i]);
 		}
